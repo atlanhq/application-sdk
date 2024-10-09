1063e19fc2950f5f303e906ec7bf652bac16baa7
Amit Prabhu <amit.dp180@gmail.com>
2024-10-09 14:07:32 +0530

    Add basic async tests

---
+++ tests/unit/paas/writers/test_json.py
@@ -0,0 +1,46 @@
+import os
+import shutil
+
+import pytest
+from application_sdk.paas.writers.json import JSONChunkedObjectStoreWriter
+
+
+class TestJSONChunkedObjectStoreWriter:
+    @staticmethod
+    @pytest.fixture
+    def writer():
+        writer = JSONChunkedObjectStoreWriter(
+            local_file_prefix="/tmp/test/test",
+            upload_file_prefix="test",
+            chunk_size=2,
+            buffer_size=1024,
+        )
+        return writer
+
+    @staticmethod
+    async def test_write(writer):
+        await writer.write({"test": "test"})
+        assert writer.current_record_count == 1
+        assert writer.total_record_count == 1
+
+    @staticmethod
+    async def test_write_list(writer):
+        await writer.write_list([{"test": "test"}, {"test": "test"}])
+        assert writer.current_record_count == 2
+        assert writer.total_record_count == 2
+
+    @staticmethod
+    async def test_close(writer):
+        await writer.write_list([
+            {"test": "test"},
+            {"test": "test"},
+            {"test": "test"},
+            {"test": "test"},
+        ])
+        await writer.close()
+        # 3 files should be created in /tmp/test
+        files = os.listdir("/tmp/test")
+        assert len(files) == 3
+        assert "test-metadata.json" in files
+        assert "test-1.json" in files
+        assert "test-2.json" in files
