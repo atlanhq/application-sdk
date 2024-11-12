e13d2b65121136b650743ede587d7e9313c2ceba
Amit Prabhu <amit.dp180@gmail.com>
2024-11-12 14:51:18 +0530

    Add query and json writer decorator with tests

--- application_sdk/workflows/sql/decorators/__init__.py
+++ application_sdk/workflows/sql/decorators/__init__.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 
-def stream_query_results(engine: Engine, query: str, chunk_size: int = 10000):
+def query_batch(engine: Engine, query: str, chunk_size: int = 10000):
     def decorator(f):
         with engine.connect() as conn:
             data_iter = pd.read_sql_query(
@@ -17,4 +17,17 @@ def stream_query_results(engine: Engine, query: str, chunk_size: int = 10000):
                     chunk_number += 1
                     del chunk_df
             return new_f
+    return decorator
+
+
+def query_write(engine: Engine, query: str, output_path: str, chunk_size: int = 10000):
+    def decorator(f):
+        @query_batch(engine, query, chunk_size)
+        async def new_fn(chunk_number, chunk_df, *args, **kwargs):
+            df: pd.DataFrame = await f(chunk_number, chunk_df, *args, **kwargs)
+            if df is None:
+                raise ValueError("Function must return a DataFrame")
+
+            df.to_json(f"{output_path}/{chunk_number}.json", orient="records", lines=True)
+        return new_fn
     return decorator
--- tests/unit/workflows/sql/decorators/test_decorators.py
+++ tests/unit/workflows/sql/decorators/test_decorators.py
@@ -1,20 +1,21 @@
 import sqlalchemy
 from sqlalchemy.sql import text
-from application_sdk.workflows.sql.decorators import stream_query_results
+from application_sdk.workflows.sql.decorators import query_batch, query_write
+import pytest
 
 
 class TestDecorators:
-    async def test_stream_query_results_basic_func(self):
+    async def test_query_batch_basic(self):
         engine = sqlalchemy.create_engine("sqlite:///:memory:")
 
-        @stream_query_results(engine, "SELECT 1")
+        @query_batch(engine, "SELECT 1")
         async def func(chunk_number, chunk_df):
             assert chunk_number == 0
             assert len(chunk_df) == 1
 
         await func()
 
-    async def test_stream_query_results_multiple_chunks(self):
+    async def test_query_batch_multiple_chunks(self):
         engine = sqlalchemy.create_engine("sqlite:///:memory:")
         with engine.connect() as conn:
             conn.execute(text("CREATE TABLE IF NOT EXISTS numbers (value INTEGER)"))
@@ -22,7 +23,7 @@ class TestDecorators:
             conn.execute(text("INSERT INTO numbers (value) VALUES (0), (1), (2)"))
             conn.commit()
 
-        @stream_query_results(engine, "SELECT * FROM numbers", chunk_size=2)
+        @query_batch(engine, "SELECT * FROM numbers", chunk_size=2)
         async def func(chunk_number, chunk_df):
             if chunk_number == 0:
                 assert len(chunk_df) == 2
@@ -30,3 +31,24 @@ class TestDecorators:
                 assert len(chunk_df) == 1
 
         await func()
+
+    async def test_query_write_basic(self):
+        engine = sqlalchemy.create_engine("sqlite:///:memory:")
+
+        @query_write(engine, "SELECT 1", "/tmp")
+        async def func(chunk_number, chunk_df):
+            assert chunk_number == 0
+            assert len(chunk_df) == 1
+            return chunk_df
+
+        await func()
+
+    async def test_query_write_no_return_func(self):
+        engine = sqlalchemy.create_engine("sqlite:///:memory:")
+
+        @query_write(engine, "SELECT 1", "/tmp")
+        async def func(chunk_number, chunk_df):
+            pass
+
+        with pytest.raises(ValueError):
+            await func()
