b5dec6b5ab031cf042c8963feb90b31d1378a129
ujala-singh <ujala2yz@gmail.com>
2024-11-25 18:25:37 +0530

    fix: Config and files

--- .github/scripts/lint_helm_chart.sh
+++ .github/scripts/lint_helm_chart.sh
@@ -2,13 +2,13 @@
 # 2. Finally creating a helm package for every chart
 
 echo "Linting the chart for secure agent components"
-cd ./charts/secure-agent-components
+cd ./atlan_charts/secure_agent_components
 helm dependency update
 helm lint
 
 cd ../../
 
 echo "Linting the chart for secure agent app"
-cd ./charts/secure-agent-app
+cd ./atlan_charts/secure_agent_apps
 helm dependency update
 helm lint
