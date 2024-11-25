5aede93240a3e496f377d2abcbd583329531a8b8
Nishchith Shetty <inishchith@gmail.com>
2024-11-24 16:20:52 -0800

    draft: miner functioning

---
+++ application_sdk/workflows/sql/workflows/miner.py
@@ -0,0 +1,203 @@
+import asyncio
+import json
+import logging
+import os
+from datetime import timedelta
+from typing import Any, Callable, Coroutine, Dict, List
+
+from temporalio import activity, workflow
+from temporalio.common import RetryPolicy
+
+from application_sdk.common.query_parallelizer import ParallelizeQueryExecutor
+from application_sdk.paas.objectstore import ObjectStore
+from application_sdk.paas.secretstore import SecretStore
+from application_sdk.paas.writers.json import JSONChunkedObjectStoreWriter
+from application_sdk.workflows.resources.temporal_resource import (
+    TemporalConfig,
+    TemporalResource,
+)
+from application_sdk.workflows.sql.resources.sql_resource import (
+    SQLResource,
+    SQLResourceConfig,
+)
+from application_sdk.workflows.sql.workflows.workflow import SQLWorkflow
+
+# from application_sdk.workflows.transformers import TransformerInterface
+from application_sdk.workflows.utils.activity import auto_heartbeater
+
+logger = logging.getLogger(__name__)
+
+
+@workflow.defn
+class SQLMinerWorkflow(SQLWorkflow):
+    fetch_queries_sql = ""
+
+    def get_activities(self) -> List[Callable[..., Any]]:
+        return [
+            self.get_query_batches,
+            self.fetch_queries,
+        ]
+
+    def store_credentials(self, credentials: Dict[str, Any]) -> str:
+        return SecretStore.store_credentials(credentials)
+
+    # def get_parallelized_chunks()
+
+    async def fetch_data(
+        self, workflow_args: Dict[str, Any], query: str, typename: str
+    ) -> int:
+        """
+        Fetch data from the database.
+
+        :param workflow_args: The workflow arguments.
+        :param query: The query to run.
+        :param typename: The type of data to fetch.
+        :return: The fetched data.
+        :raises Exception: If the data cannot be fetched.
+        """
+        output_path = workflow_args["output_path"]
+        start_marker = workflow_args["start_marker"]
+        end_marker = workflow_args["end_marker"]
+
+        raw_files_prefix = os.path.join(
+            output_path, "raw", f"{typename}_{start_marker}_{end_marker}"
+        )
+        raw_files_output_prefix = workflow_args["output_prefix"]
+
+        try:
+            async with (
+                JSONChunkedObjectStoreWriter(
+                    raw_files_prefix,
+                    raw_files_output_prefix,
+                    chunk_size=-1,  # -1 means no chunking
+                ) as raw_writer,
+            ):
+                if self.sql_resource is None:
+                    raise ValueError("SQL resource is not set")
+
+                async for batch in self.sql_resource.run_query(query, self.batch_size):
+                    # Write raw data
+                    await raw_writer.write_list(batch)
+
+                return await raw_writer.close()
+
+        except Exception as e:
+            logger.error(f"Error fetching databases: {e}")
+            raise e
+
+    @activity.defn
+    @auto_heartbeater
+    async def fetch_queries(self, workflow_args: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        Fetch and process queries from the database.
+
+        :param workflow_args: The workflow arguments.
+        :return: The fetched queries.
+        """
+        sql_query = workflow_args.get("sql_query", "")
+        # logger.info(f"Fetching queries: {workflow_args.get('chunk_index', 0)}")
+
+        chunk_count = await self.fetch_data(workflow_args, sql_query, "queries")
+        return {"typename": "queries", "chunk_count": chunk_count}
+
+    @activity.defn
+    @auto_heartbeater
+    async def get_query_batches(
+        self, workflow_args: Dict[str, Any]
+    ) -> List[Dict[str, Any]]:
+        miner_args = workflow_args.get("miner_args", {})
+
+        queries_sql_query = self.fetch_queries_sql.format(
+            database_name_cleaned=miner_args.get("database_name_cleaned", "SNOWFLAKE"),
+            schema_name_cleaned=miner_args.get("schema_name_cleaned", "ACCOUNT_USAGE"),
+            mining_start_date=miner_args.get("mining_start_date", "1731723638"),
+            crawler_last_run=miner_args.get("crawler_last_run", "1731723638"),
+        )
+
+        executor = ParallelizeQueryExecutor(
+            query=queries_sql_query,
+            timestamp_column=miner_args.get("timestamp_column", "START_TIME"),
+            chunk_size=miner_args.get("chunk_size", 200),
+            current_marker=miner_args.get("current_marker", "1731723638"),
+            sql_ranged_replace_from="ss.SESSION_CREATED_ON > TO_TIMESTAMP_TZ([MARKER], 3)",
+            sql_ranged_replace_to="ss.SESSION_CREATED_ON >= TO_TIMESTAMP_TZ([START_MARKER], 3) AND ss.SESSION_CREATED_ON <= TO_TIMESTAMP_TZ([END_MARKER], 3)",
+            ranged_sql_start_key="[START_MARKER]",
+            ranged_sql_end_key="[END_MARKER]",
+            sql_resource=self.sql_resource,
+        )
+
+        results = await executor.execute()
+
+        # Write the results to a metadata file
+        output_path = os.path.join(workflow_args["output_path"], "raw")
+        metadata_file_path = os.path.join(output_path, "queries_metadata.json")
+        os.makedirs(os.path.dirname(metadata_file_path), exist_ok=True)
+        with open(metadata_file_path, "w") as f:
+            f.write(json.dumps(results))
+
+        await ObjectStore.push_file_to_object_store(
+            workflow_args["output_prefix"], metadata_file_path
+        )
+
+        return results
+
+    @workflow.run
+    async def run(self, workflow_args: Dict[str, Any]):
+        """
+        Run the workflow.
+
+        :param workflow_args: The workflow arguments.
+        """
+        if not self.sql_resource:
+            credentials = SecretStore.extract_credentials(
+                workflow_args["credential_guid"]
+            )
+            self.sql_resource = SQLResource(SQLResourceConfig(credentials=credentials))
+
+        if not self.temporal_resource:
+            self.temporal_resource = TemporalResource(
+                TemporalConfig(application_name=self.application_name)
+            )
+
+        workflow_id = workflow_args["workflow_id"]
+        workflow.logger.info(f"Starting miner workflow for {workflow_id}")
+        retry_policy = RetryPolicy(
+            maximum_attempts=6,
+            backoff_coefficient=2,
+        )
+
+        workflow_run_id = workflow.info().run_id
+        output_prefix = workflow_args["output_prefix"]
+        output_path = f"{output_prefix}/{workflow_id}/{workflow_run_id}"
+        workflow_args["output_path"] = output_path
+        workflow_args["chunk_size"] = 200
+
+        results: List[Dict[str, Any]] = await workflow.execute_activity(  # pyright: ignore[reportUnknownMemberType]
+            self.get_query_batches,
+            workflow_args,
+            retry_policy=retry_policy,
+            start_to_close_timeout=timedelta(seconds=1000),
+        )
+
+        miner_activities: List[Coroutine[Any, Any, Dict[str, Any]]] = []
+
+        # Extract Queries
+        for result in results:
+            # Create a new copy of workflow_args for each activity
+            activity_args = workflow_args.copy()
+            activity_args["sql_query"] = result["sql"]
+            activity_args["start_marker"] = result["start"]
+            activity_args["end_marker"] = result["end"]
+
+            miner_activities.append(
+                workflow.execute_activity(  # pyright: ignore[reportUnknownMemberType]
+                    self.fetch_queries,
+                    activity_args,  # Pass the copied args
+                    retry_policy=retry_policy,
+                    start_to_close_timeout=timedelta(seconds=1000),
+                )
+            )
+
+        await asyncio.gather(*miner_activities)
+
+        workflow.logger.info(f"Miner workflow completed for {workflow_id}")
---
+++ examples/application_sql_miner.py
@@ -0,0 +1,225 @@
+"""
+This example demonstrates how to create a SQL Miner workflow for extracting query metadata from a Snowflake database.
+It uses the Temporal workflow engine to manage the extraction process.
+
+Workflow steps:
+1. Perform preflight checks
+2. Create an output directory
+3. Fetch query information
+4. Push results to object store
+
+Usage:
+1. Set the Snowflake connection credentials as environment variables
+2. Run the script to start the Temporal worker and execute the workflow
+
+Note: This example is specific to Snowflake but can be adapted for other SQL databases.
+"""
+
+import asyncio
+import logging
+import os
+import threading
+import time
+from urllib.parse import quote_plus
+
+from application_sdk.workflows.resources.temporal_resource import (
+    TemporalConfig,
+    TemporalResource,
+)
+from application_sdk.workflows.sql.builders.builder import SQLWorkflowBuilder
+from application_sdk.workflows.sql.resources.sql_resource import (
+    SQLResource,
+    SQLResourceConfig,
+)
+from application_sdk.workflows.sql.workflows.miner import SQLMinerWorkflow
+from application_sdk.workflows.sql.workflows.workflow import SQLWorkflow
+from application_sdk.workflows.transformers.atlas.__init__ import AtlasTransformer
+from application_sdk.workflows.workers.worker import WorkflowWorker
+
+APPLICATION_NAME = "snowflake"
+
+logger = logging.getLogger(__name__)
+
+
+FETCH_QUERIES_SQL = """
+WITH qs AS (
+    SELECT * FROM (
+        SELECT
+            min(start_time) AS SESSION_CREATED_ON,
+            SESSION_ID
+        FROM
+            {database_name_cleaned}.{schema_name_cleaned}.QUERY_HISTORY
+        WHERE
+            START_TIME >= CURRENT_DATE - INTERVAL '3 WEEK'
+            AND START_TIME <= TO_TIMESTAMP_TZ({crawler_last_run})
+        GROUP BY
+            SESSION_ID
+        ) ss
+        WHERE
+            ss.SESSION_CREATED_ON > TO_TIMESTAMP_TZ([START_MARKER], 3)
+            AND ss.SESSION_CREATED_ON >= TO_TIMESTAMP_TZ({mining_start_date})
+            AND ss.SESSION_CREATED_ON >= CURRENT_DATE - INTERVAL '30 DAYS'
+    ),
+    q AS (
+        SELECT
+            *,
+            CASE WHEN warehouse_size  = 'X-Small'  THEN 1
+                WHEN warehouse_size = 'Small'    THEN 2
+                WHEN warehouse_size = 'Medium'   THEN 4
+                WHEN warehouse_size = 'Large'    THEN 8
+                WHEN warehouse_size = 'X-Large'  THEN 16
+                WHEN warehouse_size = '2X-Large' THEN 32
+                WHEN warehouse_size = '3X-Large' THEN 64
+                WHEN warehouse_size = '4X-Large' THEN 128
+                ELSE 1
+            END as WAREHOUSE_PRICE
+        FROM
+            {database_name_cleaned}.{schema_name_cleaned}.QUERY_HISTORY
+        WHERE
+            EXECUTION_STATUS = 'SUCCESS'
+            AND QUERY_TYPE NOT IN
+            ('COMMIT', 'USE', 'BEGIN_TRANSACTION', 'DESCRIBE', 'ROLLBACK', 'SHOW', 'ALTER_SESSION', 'GRANT')
+            AND START_TIME <= TO_TIMESTAMP_TZ({crawler_last_run})
+            AND START_TIME >= CURRENT_DATE - INTERVAL '2 WEEK'
+    )
+    SELECT
+        q.* EXCLUDE(START_TIME, END_TIME),
+        CONVERT_TIMEZONE('UTC', q.START_TIME) as START_TIME,
+        CONVERT_TIMEZONE('UTC', q.END_TIME) as END_TIME,
+        q.QUERY_TYPE as SOURCE_QUERY_TYPE,
+        to_double(((q.execution_time / (1000 * 3600)) * q.WAREHOUSE_PRICE)) as CREDITS_USED_COMPUTE,
+        CONVERT_TIMEZONE('UTC', qs.SESSION_CREATED_ON) as SESSION_CREATED_ON,
+        s.CLIENT_VERSION,
+        s.CLIENT_BUILD_ID,
+        s.CLIENT_ENVIRONMENT,
+        s.LOGIN_EVENT_ID,
+        s.CLIENT_APPLICATION_ID,
+        s.CLIENT_APPLICATION_VERSION,
+        s.AUTHENTICATION_METHOD
+    FROM
+        q inner JOIN qs ON q.SESSION_ID = qs.SESSION_ID LEFT JOIN
+        {database_name_cleaned}.{schema_name_cleaned}.SESSIONS s ON q.SESSION_ID = s.SESSION_ID
+    ORDER BY
+        SESSION_CREATED_ON,
+        SESSION_ID,
+        START_TIME
+"""
+
+
+class SampleSQLMinerWorkflow(SQLMinerWorkflow):
+    fetch_queries_sql = FETCH_QUERIES_SQL
+
+
+class SampleSQLMinerWorkflowBuilder(SQLWorkflowBuilder):
+    def build(self, workflow: SQLWorkflow | None = None) -> SQLWorkflow:
+        return super().build(workflow=workflow or SampleSQLMinerWorkflow())
+
+
+class SnowflakeResourceConfig(SQLResourceConfig):
+    def get_sqlalchemy_connection_string(self) -> str:
+        credentials = self.credentials
+
+        encoded_password = quote_plus(credentials["password"])
+        base_url = f"snowflake://{credentials['user']}:{encoded_password}@{credentials['account_id']}"
+
+        # FIXME: add more params
+        if credentials.get("warehouse"):
+            base_url = f"{base_url}?warehouse={credentials['warehouse']}"
+        if credentials.get("role"):
+            if "?" in base_url:
+                base_url = f"{base_url}&role={credentials['role']}"
+            else:
+                base_url = f"{base_url}?role={credentials['role']}"
+
+        return base_url
+
+
+class SnowflakeResource(SQLResource):
+    default_database_alias_key = "database_name"
+    default_schema_alias_key = "name"
+
+
+async def main():
+    temporal_resource = TemporalResource(
+        TemporalConfig(
+            application_name=APPLICATION_NAME,
+        )
+    )
+    await temporal_resource.load()
+
+    transformer = AtlasTransformer(
+        connector_name=APPLICATION_NAME, connector_type="sql"
+    )
+    sql_resource = SnowflakeResource(
+        SnowflakeResourceConfig(
+            database_dialect="snowflake",
+            database_driver="snowflake",
+        )
+    )
+
+    miner_workflow: SQLWorkflow = (
+        SampleSQLMinerWorkflowBuilder()
+        .set_transformer(transformer)
+        .set_temporal_resource(temporal_resource)
+        .set_sql_resource(sql_resource)
+        .build()
+    )
+
+    worker: WorkflowWorker = WorkflowWorker(
+        temporal_resource=temporal_resource,
+        temporal_activities=miner_workflow.get_activities(),
+        workflow_classes=[SQLMinerWorkflow],
+    )
+
+    # Start the worker in a separate thread
+    worker_thread = threading.Thread(
+        target=lambda: asyncio.run(worker.start()), daemon=True
+    )
+    worker_thread.start()
+
+    # wait for the worker to start
+    time.sleep(3)
+
+    await miner_workflow.start(
+        {
+            "miner_args": {
+                "database_name_cleaned": "SNOWFLAKE",
+                "schema_name_cleaned": "ACCOUNT_USAGE",
+                "mining_start_date": "1731723638",
+                "crawler_last_run": "1732155638",
+                "chunk_size": 1000,
+                "timestamp_column": "START_TIME",
+                "SQL_REPLACE_FROM": "ss.SESSION_CREATED_ON > TO_TIMESTAMP_TZ([START_MARKER], 3)",
+                "SQL_REPLACE_TO": "ss.SESSION_CREATED_ON >= TO_TIMESTAMP_TZ([START_MARKER], 3) AND ss.SESSION_CREATED_ON <= TO_TIMESTAMP_TZ([END_MARKER], 3)",
+                "RANGED_SQL_START_KEY": "[START_MARKER]",
+                "RANGED_SQL_END_KEY": "[END_MARKER]",
+            },
+            "credentials": {
+                "account_id": os.getenv("SNOWFLAKE_ACCOUNT_ID", "localhost"),
+                "user": os.getenv("SNOWFLAKE_USER", "postgres"),
+                "password": os.getenv("SNOWFLAKE_PASSWORD", "password"),
+                "warehouse": os.getenv("SNOWFLAKE_WAREHOUSE", "compute_wh"),
+                "role": os.getenv("SNOWFLAKE_ROLE", "accountadmin"),
+            },
+            "database_driver": "snowflake",
+            "database_dialect": "snowflake",
+            "connection": {"connection": "dev"},
+            "metadata": {
+                "exclude_filter": "{}",
+                "include_filter": "{}",
+                "temp_table_regex": "",
+                "advanced_config_strategy": "default",
+                "use_source_schema_filtering": "false",
+                "use_jdbc_internal_methods": "true",
+                "authentication": "BASIC",
+                "extraction-method": "direct",
+            },
+        }
+    )
+
+    # wait for the workflow to finish
+    time.sleep(120)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
