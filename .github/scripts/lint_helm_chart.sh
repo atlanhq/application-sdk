addf1179ce7db7189101eb4883a3a90811254901
ujala-singh <ujala2yz@gmail.com>
2024-11-25 16:17:14 +0530

    fix: Helm Lint Workflow

--- .github/scripts/lint_helm_chart.sh
+++ .github/scripts/lint_helm_chart.sh
@@ -1,7 +1,14 @@
 # 1. Checking whether the charts present have sub-charts, if yes, updating them.
 # 2. Finally creating a helm package for every chart
 
-cd ./charts
+echo "Linting the chart for secure agent components"
+cd ./charts/secure-agent-components
+helm dependency update
+helm lint
 
+cd ../../
+
+echo "Linting the chart for secure agent app"
+cd ./charts/secure-agent-app
 helm dependency update
 helm lint
