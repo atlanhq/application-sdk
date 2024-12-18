7f44bc6be8f10407dffb0aedf08cc3fc08e1f0c8
Onkar Ravgan <onkar.10r@gmail.com>
2024-12-18 21:31:31 +0530

    PLT-2881 add daft support

--- application_sdk/inputs/sql_query.py
+++ application_sdk/inputs/sql_query.py
@@ -1,7 +1,8 @@
 import asyncio
 import concurrent
-from typing import Any, Iterator, Optional
+from typing import Iterator, Optional
 
+import daft
 import pandas as pd
 from sqlalchemy import text
 from sqlalchemy.engine import Engine
@@ -39,7 +40,7 @@ class SQLQueryInput(Input):
                     executor, _execute_query
                 )
         except Exception as e:
-            logger.error(f"Error reading batched data from SQL: {str(e)}")
+            logger.error(f"Error reading batched data(pandas) from SQL: {str(e)}")
 
     async def get_dataframe(self) -> pd.DataFrame:
         try:
@@ -54,10 +55,44 @@ class SQLQueryInput(Input):
                     executor, _execute_query
                 )
         except Exception as e:
-            logger.error(f"Error reading data from SQL: {str(e)}")
+            logger.error(f"Error reading data(pandas) from SQL: {str(e)}")
 
-    def get_key(self, key: str) -> Any:
-        raise AttributeError("SQLQueryInput does not support get_key method")
+    async def get_daft_dataframe(self) -> daft.DataFrame:
+        """
+        Method to read data from SQL using daft and return as daft dataframe
+        """
+        try:
+            # Daft uses ConnectorX to read data from SQL by default for supported connectors
+            # Hence it requires the connection string to be passed
+            # For unsupported connectors and if directly engine is passed, it will use SQLAlchemy
+            return daft.read_sql(
+                self.query, self.engine.url.render_as_string(hide_password=False)
+            )
+        except RuntimeError:
+            # If passing the url does not work try with the engine
+            try:
+                logger.info("Falling back to using engine to read data from SQL")
+                return daft.read_sql(self.query, lambda: self.engine.connect())
+            except Exception as e:
+                logger.error(
+                    f"Error reading data(daft) from SQL using engine: {str(e)}"
+                )
+        except Exception as e:
+            logger.error(f"Error reading data(daft) from SQL: {str(e)}")
+
+    async def get_batched_daft_dataframe(self) -> daft.DataFrame:
+        """
+        Method to read data from SQL using pandas in batches and return as daft dataframe
+        We get the data using pandas since daft does not support reading data in batches
+        This pandas data will then be converted to daft dataframe
+        """
+        try:
+            batched_df = await self.get_batched_dataframe()
+            for df in batched_df:
+                daft_df = daft.from_pandas(df)
+                yield daft_df
+        except Exception as e:
+            logger.error(f"Error reading batched data(daft) from SQL: {str(e)}")
 
 
 class AsyncSQLQueryInput(Input):
@@ -82,14 +117,48 @@ class AsyncSQLQueryInput(Input):
             async with self.async_session() as session:
                 return await session.run_sync(self._read_sql_query)
         except Exception as e:
-            logger.error(f"Error reading batched data from SQL: {str(e)}")
+            logger.error(f"Error reading batched data(pandas) from SQL: {str(e)}")
 
     async def get_dataframe(self) -> pd.DataFrame:
         try:
             async with self.async_session() as session:
                 return await session.run_sync(self._read_sql_query)
         except Exception as e:
-            logger.error(f"Error reading data from SQL: {str(e)}")
+            logger.error(f"Error reading data(pandas) from SQL: {str(e)}")
 
-    def get_key(self, key: str) -> Any:
-        raise AttributeError("SQLQueryInput does not support get_key method")
+    async def get_daft_dataframe(self) -> daft.DataFrame:
+        """
+        Method to read data from SQL using daft and return as daft dataframe
+        """
+        try:
+            # Daft uses ConnectorX to read data from SQL by default for supported connectors
+            # Hence it requires the connection string to be passed
+            # For unsupported connectors and if directly engine is passed, it will use SQLAlchemy
+            return daft.read_sql(
+                self.query, self.engine.url.render_as_string(hide_password=False)
+            )
+        except RuntimeError:
+            # If passing the url does not work try with the engine
+            try:
+                logger.info("Falling back to using engine to read data from SQL")
+                return daft.read_sql(self.query, lambda: self.engine.connect())
+            except Exception as e:
+                logger.error(
+                    f"Error reading data from SQL(daft) using engine: {str(e)}"
+                )
+        except Exception as e:
+            logger.error(f"Error reading data(daft) from SQL: {str(e)}")
+
+    async def get_batched_daft_dataframe(self) -> Iterator[daft.DataFrame]:
+        """
+        Method to read data from SQL using pandas in batches and return as daft dataframe
+        We get the data using pandas since daft does not support reading data in batches
+        This pandas data will then be converted to daft dataframe
+        """
+        try:
+            batched_df = await self.get_batched_dataframe()
+            for df in batched_df:
+                daft_df = daft.from_pandas(df)
+                yield daft_df
+        except Exception as e:
+            logger.error(f"Error reading batched data(daft) from SQL: {str(e)}")
