5295c75d646aad1ba01e7378c4913a981cc91753
Abhishek Agrawal <abhishek.agrawal@atlan.com>
2025-01-14 15:57:36 +0530

    PR#106

--- tests/unit/workflows/transformers/atlas/test_schema.py
+++ tests/unit/workflows/transformers/atlas/test_schema.py
@@ -10,7 +10,7 @@ from application_sdk.workflows.transformers.atlas import AtlasTransformer
 
 @pytest.fixture
 def resources_dir():
-    return os.path.join(os.path.dirname(__file__), "resrouces")
+    return os.path.join(os.path.dirname(__file__), "resources")
 
 
 @pytest.fixture
