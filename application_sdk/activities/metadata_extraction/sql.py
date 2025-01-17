a1314567a9e4d706d10c61e0d22642587d3f069c
SanilK2108 <sanil.khurana@atlan.com>
2025-01-17 14:05:28 +0530

    fixing comments

--- application_sdk/activities/metadata_extraction/sql.py
+++ application_sdk/activities/metadata_extraction/sql.py
@@ -134,7 +134,6 @@ class SQLMetadataExtractionActivities(ActivitiesInterface):
         if not handler:
             raise ValueError("Preflight check handler not found")
 
-        # TODO: Remove metadata
         result = await handler.preflight_check(
             {
                 "metadata": workflow_args["metadata"],
@@ -312,7 +311,6 @@ class SQLMetadataExtractionActivities(ActivitiesInterface):
         typename = kwargs.get("typename")
         workflow_id = kwargs.get("workflow_id")
         workflow_run_id = kwargs.get("workflow_run_id")
-        # TODO: Hack while using activity_input
         transformed_chunk = await self._transform_batch(
             batch_input,
             typename,
