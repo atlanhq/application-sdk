93bed7951248c56e37c111f70e5374a58bc499c9
Mustafa <mustafa.trunkwala@atlan.com>
2025-01-21 14:45:53 +0530

    Update application name environment variable

--- application_sdk/workflows/resources/constants.py
+++ application_sdk/workflows/resources/constants.py
@@ -6,4 +6,4 @@ class TemporalConstants(Enum):
     HOST = os.getenv("ATLAN_TEMPORAL_HOST", "localhost")
     PORT = os.getenv("ATLAN_TEMPORAL_PORT", "7233")
     NAMESPACE = os.getenv("ATLAN_TEMPORAL_NAMESPACE", "default")
-    APPLICATION_NAME = os.getenv("ATLAN_TEMPORAL_APPLICATION_NAME", "default")
+    APPLICATION_NAME = os.getenv("ATLAN_APPLICATION_NAME", "default")
