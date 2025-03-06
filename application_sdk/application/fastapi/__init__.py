dde23e38316a3f23021457da9e27b957d3860e5d
Mustafa <mustafa.trunkwala@atlan.com>
2025-03-06 13:46:23 +0530

    feat: load credentials in preflight check handler

--- application_sdk/application/fastapi/__init__.py
+++ application_sdk/application/fastapi/__init__.py
@@ -364,6 +364,7 @@ class FastAPIApplication(AtlanApplicationInterface):
         Returns:
             PreflightCheckResponse: Response containing preflight check results.
         """
+        await self.handler.load(body.get("credentials"))
         preflight_check = await self.handler.preflight_check(body.model_dump())
         return PreflightCheckResponse(success=True, data=preflight_check)
 
