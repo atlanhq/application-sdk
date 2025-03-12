2206baadf72dec5561b3d3f97d895b4b7f7e98aa
Abhishek Agrawal <abhishek.agrawal@atlan.com>
2025-03-12 14:34:33 +0530

    Revert "added get_configuration and update_configuration functions in the client"
    This reverts commit a1b1ea5200d95db64473d45eeb49047a108eaef7.

--- application_sdk/test_utils/e2e/base.py
+++ application_sdk/test_utils/e2e/base.py
@@ -92,8 +92,9 @@ class BaseTest(TestInterface):
             "connection": self.connection,
             "metadata": {**self.metadata, "temp-table-regex": "^temp_.*"},
         }
-        response = self.client.update_configuration(
-            workflow_id=WorkflowDetails.workflow_id, payload=update_payload
+        response = requests.post(
+            f"{self.client.host}/workflows/v1/config/{WorkflowDetails.workflow_id}",
+            json=update_payload,
         )
         self.assertEqual(response.status_code, 200)
 
@@ -102,8 +103,8 @@ class BaseTest(TestInterface):
         """
         Test configuration retrieval
         """
-        response = self.client.get_configuration(
-            workflow_id=WorkflowDetails.workflow_id
+        response = requests.get(
+            f"{self.client.host}/workflows/v1/config/{WorkflowDetails.workflow_id}"
         )
         self.assertEqual(response.status_code, 200)
 
