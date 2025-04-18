24136558725c274cba778fa65fd70b747e7d8613
Mustafa <mustafa.trunkwala@atlan.com>
2025-04-18 13:05:27 +0530

    refactor: add null check for temp table regex in query preparation

--- application_sdk/common/utils.py
+++ application_sdk/common/utils.py
@@ -57,7 +57,7 @@ def prepare_query(
         # using "or" instead of default correct defaults are set in case of empty string
         include_filter = metadata.get("include-filter") or "{}"
         exclude_filter = metadata.get("exclude-filter") or "{}"
-        if metadata.get("temp-table-regex"):
+        if metadata.get("temp-table-regex") and temp_table_regex_sql is not None:
             temp_table_regex_sql = temp_table_regex_sql.format(
                 exclude_table_regex=metadata.get("temp-table-regex")
             )
