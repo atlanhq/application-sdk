ff7799d5ae51dbcae295af86632cc1cf8680090b
Mustafa <mustafa.trunkwala@atlan.com>
2025-04-29 14:10:06 +0530

    fix: update dependabot target branch to main and correct import path in FastAPI initialization

--- application_sdk/server/fastapi/__init__.py
+++ application_sdk/server/fastapi/__init__.py
@@ -11,7 +11,7 @@ from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from uvicorn import Config, Server
 
-from application_sdk.application import AtlanApplicationInterface
+from application_sdk.server import AtlanApplicationInterface
 from application_sdk.server.fastapi.middleware.logmiddleware import LogMiddleware
 from application_sdk.server.fastapi.models import (
     FetchMetadataRequest,
