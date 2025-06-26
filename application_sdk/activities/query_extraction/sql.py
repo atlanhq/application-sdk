a9ecbee495ba18300e5382da79793a52426eb771
Abhishek Agrawal <abhishek.agrawal@atlan.com>
2025-06-26 12:21:14 +0530

    Revert "fix: update markers"
    This reverts commit b9f892e0fe82b7e40543f5f2a28d5704a5702036.

--- application_sdk/activities/query_extraction/sql.py
+++ application_sdk/activities/query_extraction/sql.py
@@ -428,9 +428,9 @@ class SQLQueryExtractionActivities(ActivitiesInterface):
             Exception: If marker file reading fails (logged as warning, not re-raised)
         """
         try:
-            #output_path = workflow_args["output_path"].rsplit("/", 2)[0]
-            #marker_file_path = os.path.join(output_path, "markerfile")
-            with open("/tmp/markers", "r") as f:
+            output_path = workflow_args["output_path"].rsplit("/", 2)[0]
+            marker_file_path = os.path.join(output_path, "markerfile")
+            with open(marker_file_path, "r") as f:
                 current_marker = f.read()
             return int(current_marker)
         except Exception as e:
