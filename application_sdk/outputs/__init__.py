c7aff25ffa270afe63a07850cd5990b1ffa900e0
Abhishek Agrawal <abhishek.agrawal@atlan.com>
2025-10-03 21:24:53 +0530

    fix: miner-marker-file-name-creation-logic

--- application_sdk/outputs/__init__.py
+++ application_sdk/outputs/__init__.py
@@ -96,7 +96,6 @@ class Output(ABC):
         chunk_count: Optional[int] = None,
         chunk_part: int = 0,
         start_marker: Optional[str] = None,
-        end_marker: Optional[str] = None,
     ) -> str:
         """Generate a file path for a chunk.
 
@@ -110,10 +109,11 @@ class Output(ABC):
             str: Generated file path for the chunk.
         """
         # For Query Extraction - use start marker
-        if chunk_count is None:
-            return f"atlan_raw_mined_{str(start_marker)}_{str(chunk_part)}{self._EXTENSION}"
-        else:
-            return f"atlan_raw_mined_{str(start_marker)}_{str(chunk_count)}_{str(chunk_part)}{self._EXTENSION}"
+        if start_marker:
+            if chunk_count is None:
+                return f"atlan_raw_mined_{str(start_marker)}_{str(chunk_part)}{self._EXTENSION}"
+            else:
+                return f"atlan_raw_mined_{str(start_marker)}_{str(chunk_count)}_{str(chunk_part)}{self._EXTENSION}"
 
         # For regular chunking - include chunk count
         if chunk_count is None:
@@ -215,7 +215,7 @@ class Output(ABC):
                     self.current_buffer_size_bytes + chunk_size_bytes
                     > self.max_file_size_bytes
                 ):
-                    output_file_name = f"{self.output_path}/{self.path_gen(self.chunk_count, self.chunk_part, self.start_marker, self.end_marker)}"
+                    output_file_name = f"{self.output_path}/{self.path_gen(self.chunk_count, self.chunk_part, self.start_marker)}"
                     if os.path.exists(output_file_name):
                         await self._upload_file(output_file_name)
                         self.chunk_part += 1
@@ -229,7 +229,7 @@ class Output(ABC):
 
             if self.current_buffer_size_bytes > 0:
                 # Finally upload the final file to the object store
-                output_file_name = f"{self.output_path}/{self.path_gen(self.chunk_count, self.chunk_part)}"
+                output_file_name = f"{self.output_path}/{self.path_gen(self.chunk_count, self.chunk_part, self.start_marker)}"
                 if os.path.exists(output_file_name):
                     await self._upload_file(output_file_name)
                     self.chunk_part += 1
@@ -363,9 +363,7 @@ class Output(ABC):
         try:
             if not is_empty_dataframe(chunk):
                 self.total_record_count += len(chunk)
-                output_file_name = (
-                    f"{self.output_path}/{self.path_gen(self.chunk_count, chunk_part)}"
-                )
+                output_file_name = f"{self.output_path}/{self.path_gen(self.chunk_count, chunk_part, self.start_marker)}"
                 await self.write_chunk(chunk, output_file_name)
 
                 self.current_buffer_size = 0
